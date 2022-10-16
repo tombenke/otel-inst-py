c4ff1a3ac3ba8dc9ce05a0c47200d5c5e7e221bf
Tamás Benke <tombenke@gmail.com>
2022-10-16 15:54:58 +0200

    Add examples with tracing using default config

--- examples/simple_trace_stdout.py
+++ examples/simple_trace_stdout.py
@@ -24,6 +24,8 @@ oti = OTI(
 tracer = trace.get_tracer(__name__)
 with tracer.start_as_current_span("the-main-span-of-the-example") as span:
     # do some work that 'span' will track
+    if span.is_recording():
+        span.set_attribute("notes", "This note should appear as a span attribute.")
     print(f"TRACER / SPAN is executed: {span.get_span_context().trace_id}")
     # When the 'with' block goes out of scope, 'span' is closed for you
 
