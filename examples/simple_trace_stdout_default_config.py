a36358c6adbde0d6df4d9380e40f24b1b73efb36
Tamás Benke <tombenke@gmail.com>
2022-10-16 13:11:31 +0200

    Add example with default OTEL SDK config

---
+++ examples/simple_trace_stdout_default_config.py
@@ -0,0 +1,27 @@
+"""
+This example uses the `OTIConfig` with default values.
+
+The program uses the tracing API, but there is no trace exported to gRPC
+neither to `stdout` because the corresponding default config values are:
+
+```bash
+DEFAULT_EXPORTER_TYPE = "STDOUT"
+DEFAULT_OTEL_SAMPLING_TYPE = "NEVER"
+```
+
+"""
+from opentelemetry import trace  # Import the OTEL API
+from oti import OTI, OTIConfig, ExporterConfig, SamplingConfig
+
+# Configure the OTEL SDK
+oti = OTI()
+
+# Use the OTEL via API only
+tracer = trace.get_tracer(__name__)
+with tracer.start_as_current_span("the-main-span-of-the-example") as span:
+    # do some work that 'span' will track
+    print(f"TRACER / SPAN is executed: {span.get_span_context().trace_id}")
+    # When the 'with' block goes out of scope, 'span' is closed for you
+
+# Shut down the OTEL SDK
+oti.shutdown()
